38b2e41b7adad308e6095f8ee02dcb31ada0a971
Starforce6000 <joshwilson928@gmail.com>
2022-12-04 11:15:13 -0600

    shift added

--- Iteration2/UIDemo/Iteration2/src/main/java/Availability.java
+++ Iteration2/UIDemo/Iteration2/src/main/java/Availability.java
@@ -1,3 +1,12 @@
-public class Avialability {
+public class Availability {
+    Day day;
+    Shift shift;
 
+    public Day getDay() {
+        return day;
+    }
+
+    public Shift getShift() {
+        return shift;
+    }
 }
