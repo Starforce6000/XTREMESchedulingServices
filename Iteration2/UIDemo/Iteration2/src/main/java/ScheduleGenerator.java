9a0c738a7cc338bb7fe48ec071dd4d57598aaf4b
Starforce6000 <joshwilson928@gmail.com>
2022-12-04 12:29:17 -0600

    fixed equality operator

--- Iteration2/UIDemo/Iteration2/src/main/java/ScheduleGenerator.java
+++ Iteration2/UIDemo/Iteration2/src/main/java/ScheduleGenerator.java
@@ -1,5 +1,16 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class ScheduleGenerator {
     Department department;
-
     Schedule schedule;
+    List<EmployeeSchedule> employeeSchedules = new LinkedList<EmployeeSchedule>();
+
+    public ScheduleGenerator(Department d) {
+        Schedule newSchedule = new Schedule();
+
+        for(Employee employee : department.getEmployees()) {
+            EmployeeSchedule eSchedule = new EmployeeSchedule(employee, newSchedule);
+        }
+    }
 }
