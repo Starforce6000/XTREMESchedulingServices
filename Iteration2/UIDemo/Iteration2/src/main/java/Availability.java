55810d598ae8400466221e7ec64c7c96d8721bee
Brad Buckingham <bradbuckingham@Brads-MacBook-Pro.local>
2022-12-04 12:23:20 -0600

    updated the employeeDao, Employee, and Availability classes

--- Iteration2/UIDemo/Iteration2/src/main/java/Availability.java
+++ Iteration2/UIDemo/Iteration2/src/main/java/Availability.java
@@ -4,6 +4,18 @@ import Enums.Shift;
 public class Availability {
     Day day;
     Shift shift;
+    Availability(Day day, Shift shift){
+        this.day = day;
+        this.shift = shift;
+    }
+
+    public void setDay(Day day) {
+        this.day = day;
+    }
+
+    public void setShift(Shift shift) {
+        this.shift = shift;
+    }
 
     public Day getDay() {
         return day;
--- Iteration2/UIDemo/Iteration2/src/main/java/Employee.java
+++ Iteration2/UIDemo/Iteration2/src/main/java/Employee.java
@@ -2,15 +2,50 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
+    int id;
     String name;
     Department department;
+    String email;
     List<Availability> availabilityList = new LinkedList<Availability>();
 
-    public List<Availability> getAvailability() {
-        return availabilityList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAvailabilityList(List<Availability> availabilityList) {
+        this.availabilityList = availabilityList;
+    }
+    public List<Availability> getAvailability() {
+        return availabilityList;
+    }
+
 }
