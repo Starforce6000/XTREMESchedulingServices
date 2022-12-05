8cd7e252616bf310cca86cd8e635d45decd47faa
Brad Buckingham <bradbuckingham@Brads-MacBook-Pro.local>
2022-12-05 16:50:07 -0600

    test

--- Iteration2/UIDemo/Iteration2/src/main/java/app/addEmployeePanel.java
+++ Iteration2/UIDemo/Iteration2/src/main/java/app/addEmployeePanel.java
@@ -3,6 +3,7 @@ package app;
 import Models.Department;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class addEmployeePanel extends JPanel {
 //    int id;
@@ -16,5 +17,21 @@ public class addEmployeePanel extends JPanel {
 //    String password;
     JLabel id, name, department, email, day, shift, password;
     JTextField id_field, name_field, email_field, password_field;
+    JComboBox department_box, day_box, shift_box;
+    ArrayList<String> departments = new ArrayList<>();
+    public addEmployeePanel(ArrayList<String> departments){
+        id = new JLabel("ID: ");
+        name = new JLabel("Name: ");
+        department = new JLabel("Department: ");
+        email = new JLabel("Email: ");
+        day = new JLabel("Day: ");
+        shift = new JLabel("Shift: ");
+        password = new JLabel("Password: ");
+        id_field = new JTextField(10);
+        name_field= new JTextField(10);
+        email_field= new JTextField(10);
+        password_field= new JTextField(10);
+
+    }
 
 }
