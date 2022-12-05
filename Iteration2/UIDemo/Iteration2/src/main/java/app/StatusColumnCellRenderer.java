3e68a115e21497f8265b896a53ff86dc642a63c8
Nick C <nick_cook1@baylor.edu>
2022-12-04 21:49:01 -0600

    WE ADDED COLOR..

---
+++ Iteration2/UIDemo/Iteration2/src/main/java/app/StatusColumnCellRenderer.java
@@ -0,0 +1,31 @@
+package app;
+
+import Enums.Shift;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+
+class StatusColumnCellRenderer extends DefaultTableCellRenderer {
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+
+        //Cells are by default rendered as a JLabel.
+        JLabel l = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+
+        //Get the status for the current row.
+        if (value.equals(Shift.Day)) {
+            l.setBackground(new Color(61,133,198));
+        } else if(value.equals(Shift.Swing)){
+            l.setBackground(new Color(11, 83, 148));
+        }else if(value.equals(Shift.Night)){
+            l.setBackground(new Color(7, 55, 99));
+        }else{
+            l.setBackground(Color.GREEN);
+        }
+
+        //Return the JLabel which renders the cell.
+        return l;
+
+    }
+}
