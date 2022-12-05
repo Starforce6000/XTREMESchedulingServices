aca0e16aff62e8db06f945dc14548cd302a86346
Nick C <nick_cook1@baylor.edu>
2022-12-04 21:42:21 -0600

    Finishing IO Testing

--- Iteration2/UIDemo/Iteration2/src/test/java/IOTesting/FileInput.java
+++ Iteration2/UIDemo/Iteration2/src/test/java/IOTesting/FileInput.java
@@ -1,29 +1,34 @@
 package IOTesting;
 
+import DAO.*;
 import org.junit.jupiter.api.*;
-
 import java.io.*;
+import static org.junit.Assert.*;
 
 public class FileInput {
-    BufferedReader stream;
+    EmployeeDAO dao;
 
     @BeforeEach
     void init(){
-
+        dao = new EmployeeDAO();
     }
 
     @Test
     void somethingBroke(){
-
+        try {
+            dao.loadEmployeesFromFile(new File("noWorky.csv"));
+        }catch (IOException e){
+            assertEquals(true, true);
+        }
     }
 
     @Test
-    void somethingProper(){}
-
-    @Test
-    void somethingBorrowed(){}
-
-    @Test
-    void somethingBlue(){}
+    void somethingProper(){
+        try {
+            dao.loadEmployeesFromFile(new File("employee.csv"));
+        }catch (IOException e){
+            assertEquals(false, true);
+        }
+    }
 
 }
--- Iteration2/UIDemo/Iteration2/src/test/java/IOTesting/FileOutput.java
+++ Iteration2/UIDemo/Iteration2/src/test/java/IOTesting/FileOutput.java
@@ -1,4 +1,54 @@
 package IOTesting;
 
+import DAO.*;
+import Enums.*;
+import Models.*;
+import app.*;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class FileOutput {
+    EmployeeDAO dao;
+    ArrayList<Employee> yaMan = new ArrayList<>();
+    ArrayList<Day> days = new ArrayList<>();
+
+    @BeforeEach
+    void init(){
+        dao = new EmployeeDAO();
+        days.add(Day.MONDAY);
+        Employee e = new Employee();
+        Availability a = new Availability(days,Shift.Day);
+        e.setEmail("j@j.com");
+        e.setName("e");
+        e.setAvailability(a);
+        e.setManager(false);
+        e.setId(0);
+        e.setPassword("1234");
+        e.setDepartment(new Department());
+    }
+
+    @Test
+    void somethingBroke(){
+        try {
+            dao.saveEmployeesToFile(null);
+        }catch (NullPointerException e){
+            assertEquals(true, true);
+        }catch (IOException ex){
+
+        }
+    }
+
+    @Test
+    void somethingProper(){
+        try {
+            dao.saveEmployeesToFile(yaMan);
+        }catch (IOException e){
+            assertEquals(false, true);
+        }
+    }
 }
